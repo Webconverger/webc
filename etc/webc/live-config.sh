3f89839a4bd305e6ab913a20cb07d969aeeaed1a
root <root@h2.(none)>
2012-03-15 07:26:12 +0000

    added all extensions, logging to live-config

--- etc/webc/live-config.sh
+++ etc/webc/live-config.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 . /etc/webc/webc.conf
 
-cmdline=""
-while test "$cmdline" = ""; do
-	wget -q -O /etc/webc/cmdline.tmp $config_url
-	cmdline="$( cat /etc/webc/cmdline.tmp )" 
-done
-mv /etc/webc/cmdline.tmp /etc/webc/cmdline
-
-
+fix_chrome() 
+{
 link="/usr/lib/iceweasel/extensions/webconverger"
 
 for x in $( cmdline ); do
@@ -16,9 +10,23 @@ for x in $( cmdline ); do
 	webcchrome=*) 
 		chrome=${x#webcchrome=}
 		test -e $link && rm -f $link 
+		logs "switching chrome to ${chrome}"
 		ln -s "/etc/webc/iceweasel/extensions/${chrome}" $link
 		;;
 	esac
 done
+}
 	
+
+fix_chrome  
+
+cmdline=""
+while test "$cmdline" = ""; do
+	wget -q -O /etc/webc/cmdline.tmp $config_url
+	cmdline="$( cat /etc/webc/cmdline.tmp )" 
+done
+mv /etc/webc/cmdline.tmp /etc/webc/cmdline
+
+fix_chrome # user may have changed it
+
 exec sleep 3600
