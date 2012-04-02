bd379487ec5f69d4ccd48853581de4abd8bd7980
root <root@h2.(none)>
2012-04-02 06:29:45 +0000

    installer cleaned up

--- etc/webc/install-to-disk.sh
+++ etc/webc/install-to-disk.sh
@@ -1,110 +1,138 @@
-#!/bin/sh
+#!/bin/bash
 . "/etc/webc/webc.conf"
-set -x
-exec 2>&1
-if cmdline_has debug; then
-	set -x
-else
-	exec > /tmp/install.log
-fi
+exec &> /root/install.log
+
+clear_screen() {
+	for i in `seq 200`; do
+		echo > /dev/console
+	done
+}
+add_root() {
+	sed -i 's/^root:x:\(.*\)/root::\1/' /etc/passwd
+}
+
+_logs() {
+	echo "$@" > /dev/console
+}
+_err() {
+	_logs "ERR:" "$@"
+}
 
 find_disk() {
-	logs "finding disk"
+	_logs "finding disk"
 	for disk in /dev/hda /dev/sda; do
 		test -e $disk && { echo $disk ; return 0; }
 	done
 }
 partition_disk() {
 	local disk="$1"
-	logs "partitioning ${disk}"
+	_logs "partitioning ${disk}"
 	cat<<EOF | sed -e "s#DISK#${disk}#" |/sbin/sfdisk $disk 
 unit: sectors
 
-DISK1 : start=     2048, size=   3145728, Id=83, bootable
+DISK1 : start=     2048, Id=83, bootable
 EOF
 }
 verify_partition() {
 	local disk="$1"
-	logs "verifying partitions on ${disk}"
+	_logs "verifying partitions on ${disk}"
 	/sbin/sfdisk -V -q $disk && return 0
 	# scream / abort?
 }	
 install_extlinux() {
 	local dir="$1"
-	logs "installing extlinux to ${dir}"
-	extlinux --install "${dir}/extlinux"
+	local disk="$2"
+	_logs "installing extlinux to ${dir}"
+
+	extlinux --install ${dir}
+	rm -f ${dir}/boot.txt
+	test -e ${dir}/ldlinux.sys || _err "extlinux install failed"
+
+cat<<EOF >> ${dir}/linux.cfg
+label fail
+	linux /boot/vmlinuz-old 
+	append initrd=/boot/initrd.img-old quiet
+EOF
+
+sed -i \
+	-e 's/^\(prompt\).*/\1 0/' \
+	-e 's/^\(timeout\).*/\1 5/' \
+	-e 's/^\(display\).*//' \
+	${dir}/extlinux.conf 
+
+sed -i \
+	-e 's|\(append.*\)|\1 root='$disk' quiet silent |' \
+	${dir}/linux.cfg
+
+	( cd ${dir}/.. && ln -s . boot )
+
 }
 install_root() {
 	local dir="$1"
-	logs "installing root to ${dir}"
+	_logs "installing root to ${dir}, this will take several minutes"
 	unsquashfs -f -n -d $dir /live/image/live/filesystem.squashfs 
 }
 user_setup() {
 	local dir="$1"
-	logs "user setup"
+	_logs "user setup"
 	chroot $dir groupadd webc
 	chroot $dir useradd -g webc webc
 	chroot $dir chown -R webc:webc /home/webc
+	grep -qs '^webc' ${dir}/etc/passwd || {
+		_err "user setup failed"
+	}
+}
+
+install_files() {
+	dir="$1"
+	disk="$2"
+	sed -i 's/^root::\(.*\)/root:x:\1/' ${dir}/etc/passwd
+
+	sed -i \
+		-e 's/^.sudo.*/webc ALL=NOPASSWD: ALL/' \
+		${dir}/etc/sudoers 
+
+	cat<<EOF > ${dir}/etc/fstab
+proc /proc proc defaults 0 0
+${disk} / ext3 defaults 0 0
+/swap none swap sw 0 0
+EOF
+
+
+	cat<<EOF > ${dir}/etc/hosts
+127.0.0.1	localhost localhost.localdomain
+EOF
+
+	cat<<EOF >> ${dir}/etc/network/interfaces
+auto eth0
+EOF
+
 }
 
 ! grep -qs install /proc/cmdline && exec sleep 86400
-while ! wget -q -O /etc/webc/cmdline $config_url; do sleep 5; done
+clear_screen
+cmdline_has debug && echo "debug has been enabled" >/dev/console
+add_root
 disk=$( find_disk )
 partition_disk $disk
 verify_partition $disk
-mke2fs -j ${disk}1
+partition="${disk}1"
+mke2fs -j $partition
 test -d /mnt/root || mkdir /mnt/root
-mount ${disk}1 /mnt/root
+mount $partition /mnt/root
 dd if=/dev/zero of=/mnt/root/swap bs=1M count=256
 mkswap /mnt/root/swap
 swapon /mnt/root/swap
 install_root /mnt/root
-install_extlinux /mnt/root/
-
-( cd /mnt/root/boot && ln -s . boot )
-
-cat<<EOF >> /mnt/root/boot/extlinux/extlinux.conf
-include stdmenu.cfg
-default l0
-EOF
-
-
-sed -i \
-	-e 's/^\(prompt\).*/\1 0/' \
-	-e 's/^\(timeout\).*/\1 2/' \
-	/mnt/root/boot/extlinux/extlinux.conf 
-
-sed -i \
-	-e 's#\(append.*\)#\1 root=/dev/sda3 quiet silent #' \
-	/mnt/root/boot/extlinux/linux.cfg
-
-sed -i \
-	-e 's/^.sudo.*/webc ALL=NOPASSWD: ALL/' \
-	/mnt/root/etc/sudoers 
-
-logs "adding fstab"
-cat<<EOF > /mnt/root/etc/fstab
-proc /proc proc defaults 0 0
-/dev/sda1 / ext3 defaults 0 0
-/swap none swap sw 0 0
-EOF
-
+install_files /mnt/root $partition
 user_setup /mnt/root
-grep -qs '^webc' /mnt/root/etc/passwd || {
-	logs "user setup failed"
-}
+install_extlinux /mnt/root/boot/extlinux $partition
 
-cat<<EOF > /mnt/root/etc/hosts
-127.0.0.1	localhost localhost.localdomain
-EOF
-
-cat<<EOF >> /etc/network/interfaces
-auto eth0
-EOF
-
-cp /etc/webc/cmdline /mnt/root/etc/webc/cmdline
-
-logs "umount'ing partitions"
-umount ${disk}1
-logs "install complete"
+_logs "umount'ing partitions"
+umount /mnt/root
+_logs "install complete"
+if cmdline_has debug; then
+	exec sleep 86400
+fi
+_logs "rebooting..."
 /sbin/reboot
