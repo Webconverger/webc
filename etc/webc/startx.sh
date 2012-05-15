176e5697dcd8601bedab65d165a4aff3333a85b9
Kai Hendry <hendry@webconverger.com>
2012-05-15 10:46:55 +0000

    Added nvidia detection to etc/webc/startx.sh, fixing #30

--- etc/webc/startx.sh
+++ etc/webc/startx.sh
@@ -2,6 +2,28 @@
 
 source /etc/webc/webc.conf
 
-cmdline_has nvidia && nvidia-xconfig --randr-rotation -s
+nvdetect() {
+
+NV_DEVICES=$(lspci -mn | awk '{ gsub("\"",""); if ($2 == "0300" && ($3 == "10de" || $3 == "12d2")) { print $1 } }')
+
+if test -z "$NV_DEVICES"
+then
+	logs "No NVIDIA GPU detected."
+	return 1
+fi
+
+NVGA=$(lspci -mn -s ${NV_DEVICES%% *} | awk '{ gsub("\"",""); print $3 $4 }')
+
+if grep -il $NVGA /usr/share/nvidia/*.ids | grep -q legacy
+then
+	logs "Legacy NVIDIA GPU detected"
+	return 1
+fi
+
+return 0
+
+}
+
+nvdetect && nvidia-xconfig --randr-rotation >/home/webc/.xerrors 2>&1
 cmdline_has debug || cat /etc/webc/xorg.conf >> /etc/X11/xorg.conf
 exec su webc -c startx >/home/webc/.xerrors 2>&1
