1f64e64884f190dd0adf9954a52ac68a3d817f97
Kai Hendry <hendry@webconverger.com>
2012-06-25 14:29:43 +0000

    restore DDG link after iceweasel packaging nuked it

--- usr/share/iceweasel/defaults/preferences/firefox.js
+++ usr/share/iceweasel/defaults/preferences/firefox.js
@@ -187,7 +187,7 @@ pref("lightweightThemes.update.enabled", true);
 pref("keyword.enabled", true);
 // Override the default keyword.URL. Empty value means
 // "use the search service's default engine"
-pref("keyword.URL", "");
+pref("keyword.URL", "https://duckduckgo.com/?t=webconverger&q=");
 
 pref("general.skins.selectedSkin", "classic/1.0");
 
