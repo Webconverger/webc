498e4936f7657dd76f3070aa740cf7dd3fe621f4
Kai Hendry <hendry@webconverger.com>
2012-06-25 17:12:12 +0000

    Copied in pref on matthijskooijman's suggestion https://github.com/Webconverger/webc/commit/1f64e64884f190dd0adf9954a52ac68a3d817f97#commitcomment-1499948

--- etc/iceweasel/pref/iceweasel.js
+++ etc/iceweasel/pref/iceweasel.js
@@ -1,3 +1,4 @@
+pref("keyword.URL", "https://duckduckgo.com/?t=webconverger&q=");
 lockPref("xpinstall.enabled" ,false);
 lockPref("extensions.autoDisableScopes", 0);
 lockPref("extensions.shownSelectionUI", true);
