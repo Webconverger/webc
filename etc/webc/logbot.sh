2efa4778421e00d850a30ee873c6781bfd9026e2
Kai Hendry <hendry@webconverger.com>
2012-08-09 10:28:27 +0000

    Re-organisation due to noexec limitation on /home
    Appeared to work before because /home was remounted with "noexec" _after_ .xinitrc was started on the first X invocation and
    xinit does a fallback to running /bin/sh when .xinitrc fails due to noexec on the second X run.
    
    Despite the "noexec" flag applied on home "/home/webc/webc.sh" is placed in /home because it is run by the "webc" user and seems like the most natural place for it.
    http://serverfault.com/questions/72356/how-useful-is-mounting-tmp-noexec
    
    Now webc.sh (originally .xinitrc) is the main "loop" of Webconverger, which in turn triggers live-config.sh/updates.sh to do tasks of a higher privilege.

--- etc/webc/logbot.sh
+++ etc/webc/logbot.sh
@@ -3,6 +3,12 @@
 
 ! cmdline_has debug && { "$@"; exit 0; }
 
-"$@" 2>&1 | while read line; do
-	logger -t $1 -p syslog.debug -- "$line"
+"$@" 2>&1 | while read line
+do
+	if test "$1" = "bash" # For the "bash -x webc.sh" case
+	then
+		logger -t $3 -p syslog.debug -- "$line"
+	else
+		logger -t $1 -p syslog.debug -- "$line"
+	fi
 done
