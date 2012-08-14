9d4dc10b0b0ece85459061ea0ebb621990057d9b
Kai <hendry@webconverger.com>
2012-08-14 14:29:03 +0000

    Package list cleanup and added Firefox 14

--- etc/iceweasel/pref/iceweasel.js
+++
@@ -1,5 +0,0 @@
-lockPref("keyword.URL", "https://duckduckgo.com/?t=webconverger&q=");
-lockPref("xpinstall.enabled" ,false);
-lockPref("extensions.autoDisableScopes", 0);
-lockPref("extensions.shownSelectionUI", true);
-lockPref("intl.locale.matchOS", false);
