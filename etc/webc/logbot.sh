0f1b615b4ca3040d9d181ffdb2a8992fdd610539
Matthijs Kooijman <matthijs@stdin.nl>
2012-10-01 16:00:10 +0200

    Restructure logbot.sh to use exec
    This will cause one less process to be running. The new structure is
    also a bit more clear.

--- etc/webc/logbot.sh
+++ etc/webc/logbot.sh
@@ -2,14 +2,18 @@
 . /etc/webc/functions.sh
 . /etc/webc/webc.conf
 
-! cmdline_has debug && { "$@" >/dev/null 2>/dev/null; exit 0; }
+if !cmdline_has debug; then
+	exec "$@" >/dev/null 2>/dev/null
+else
+	exec "$@" 2>&1 | while read line
+	do
+		if test "$1" = "bash" # For the "bash -x webc.sh" case
+		then
+			logger -t $3 -p syslog.debug -- "$line"
+		else
+			logger -t $1 -p syslog.debug -- "$line"
+		fi
+	done
+fi
 
-"$@" 2>&1 | while read line
-do
-	if test "$1" = "bash" # For the "bash -x webc.sh" case
-	then
-		logger -t $3 -p syslog.debug -- "$line"
-	else
-		logger -t $1 -p syslog.debug -- "$line"
-	fi
-done
+# This is never reached!
