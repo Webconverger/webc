c52bdcb09d5f56d86ff7e72efbd0c7de36f3511a
Kai Hendry <hendry@webconverger.com>
2013-06-26 09:19:09 +0000

    Locales updated, thanks to sane Archlinux packaging: https://projects.archlinux.org/svntogit/packages.git/tree/trunk?h=packages/firefox-i18n

--- opt/firefox/langpacks/langpack-gu-IN@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++ opt/firefox/langpacks/langpack-gu-IN@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -1,5 +1,5 @@
-//@line 36 "/builds/slave/rel-m-rel-lx_rpk_3-00000000000/mozilla-release/gu-IN/browser/firefox-l10n.js"
+//@line 4 "/builds/slave/rel-m-rel-lx_rpk_3-00000000000/mozilla-release/gu-IN/browser/firefox-l10n.js"
 
-//@line 38 "/builds/slave/rel-m-rel-lx_rpk_3-00000000000/mozilla-release/gu-IN/browser/firefox-l10n.js"
+//@line 6 "/builds/slave/rel-m-rel-lx_rpk_3-00000000000/mozilla-release/gu-IN/browser/firefox-l10n.js"
 
 pref("general.useragent.locale", "gu-IN");
