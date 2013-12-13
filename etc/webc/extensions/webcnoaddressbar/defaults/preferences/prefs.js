d3622298c3f52d05141de19f0062a8b8131e7ea1
aga-git <root@agah.ru>
2013-12-13 13:41:57 +0000

    Chaged webconverger.js, unchanged webcnoaddressbar prefs

--- etc/webc/extensions/webcnoaddressbar/defaults/preferences/prefs.js
+++ etc/webc/extensions/webcnoaddressbar/defaults/preferences/prefs.js
@@ -59,11 +59,3 @@ pref("network.protocol-handler.external.news", false);
 pref("network.protocol-handler.external.nntp", false);
 pref("network.protocol-handler.external.snews", false);
 
-//AGA
-pref("browser.newtab.url","http://shops.amag.ru");
-pref("browser.feeds.showFirstRunUI", false);
-pref("browser.rights.3.shown", true);
-pref("datareporting.healthreport.service.firstRun", false);
-pref("datareporting.healthreport.uploadEnabled", false);
-pref("datareporting.healthreport.service.enabled", false);
-pref("datareporting.policy.dataSubmissionEnabled", false);
