7585e5068fb81bfb95350ded56b729602fe8c127
Kai Hendry <hendry@webconverger.com>
2014-09-03 06:10:43 +0000

    Firefox 32 https://www.mozilla.org/en-US/firefox/32.0/releasenotes/

--- opt/firefox/langpacks/langpack-bg@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++ opt/firefox/langpacks/langpack-bg@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -1,5 +1,5 @@
-//@line 36 "/builds/slave/rel-m-rel-lx_rpk_1-00000000000/l10n/bg/browser/firefox-l10n.js"
+//@line 4 "/builds/slave/rel-m-rel-lx_rpk_1-00000000000/l10n/bg/browser/firefox-l10n.js"
 
-//@line 38 "/builds/slave/rel-m-rel-lx_rpk_1-00000000000/l10n/bg/browser/firefox-l10n.js"
+//@line 6 "/builds/slave/rel-m-rel-lx_rpk_1-00000000000/l10n/bg/browser/firefox-l10n.js"
 
 pref("general.useragent.locale", "bg");
--- opt/firefox/langpacks/langpack-sv-SE@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++ opt/firefox/langpacks/langpack-sv-SE@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -3,4 +3,3 @@
 //@line 6 "/builds/slave/rel-m-rel-lx_rpk_9-00000000000/l10n/sv-SE/browser/firefox-l10n.js"
 
 pref("general.useragent.locale", "sv-SE");
-pref("browser.fixup.alternate.suffix", ".se");
