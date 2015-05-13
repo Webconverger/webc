b04b99dc05f160b12cbc01ef8bac90db797ee18a
Kai Hendry <hendry@webconverger.com>
2015-05-13 08:40:21 +0000

    https://www.mozilla.org/en-US/firefox/38.0/releasenotes/ https://www.mozilla.org/en-US/security/known-vulnerabilities/firefox/#firefox38

--- opt/firefox/langpacks/langpack-ga-IE@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++ opt/firefox/langpacks/langpack-ga-IE@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -1,5 +1,5 @@
-//@line 36 "/builds/slave/rel-m-rel-lx_rpk_4-00000000000/l10n/ga-IE/browser/firefox-l10n.js"
+//@line 4 "/builds/slave/rel-m-rel-lx_rpk_4-00000000000/l10n/ga-IE/browser/firefox-l10n.js"
 
-//@line 38 "/builds/slave/rel-m-rel-lx_rpk_4-00000000000/l10n/ga-IE/browser/firefox-l10n.js"
+//@line 6 "/builds/slave/rel-m-rel-lx_rpk_4-00000000000/l10n/ga-IE/browser/firefox-l10n.js"
 
 pref("general.useragent.locale", "ga-IE");
