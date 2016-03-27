da4a506c8f592f6c22c6b582ae2b5a9b25f03b67
Kai Hendry <hendry@webconverger.com>
2016-03-27 06:54:15 +0000

    https://www.mozilla.org/en-US/firefox/45.0.1/releasenotes/

---
+++ opt/firefox/langpacks/langpack-gn@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,8 @@
+//@line 4 "/builds/slave/rel-m-rel-lx_rpk_5-00000000000/l10n/gn/browser/firefox-l10n.js"
+
+//@line 6 "/builds/slave/rel-m-rel-lx_rpk_5-00000000000/l10n/gn/browser/firefox-l10n.js"
+
+//@line 9 "/builds/slave/rel-m-rel-lx_rpk_5-00000000000/l10n/gn/browser/firefox-l10n.js"
+pref("browser.search.geoSpecificDefaults", true);
+
+pref("general.useragent.locale", "gn");
