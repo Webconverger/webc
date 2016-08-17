78977f364769585f1383678795d3673c57982bba
Kai Hendry <hendry@webconverger.com>
2016-08-17 11:41:14 +0000

    Firefox ESR 45.3.0 https://www.mozilla.org/en-US/firefox/45.3.0/releasenotes/ because addon signing in FF48 makes life too painful

--- opt/firefox/langpacks/langpack-hr@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++ opt/firefox/langpacks/langpack-hr@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -1,5 +1,5 @@
-//@line 4 "/builds/slave/rel-m-rel_fx_lx_l10n_rpk-00000/build/l10n/hr/browser/firefox-l10n.js"
+//@line 4 "/builds/slave/rel-m-esr45_fx_lx_l10n_rpk-000/build/l10n/hr/browser/firefox-l10n.js"
 
-//@line 6 "/builds/slave/rel-m-rel_fx_lx_l10n_rpk-00000/build/l10n/hr/browser/firefox-l10n.js"
+//@line 6 "/builds/slave/rel-m-esr45_fx_lx_l10n_rpk-000/build/l10n/hr/browser/firefox-l10n.js"
 
 pref("general.useragent.locale", "hr");
--- opt/firefox/langpacks/langpack-sk@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++ opt/firefox/langpacks/langpack-sk@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -1,6 +1,6 @@
-//@line 4 "/builds/slave/rel-m-rel_fx_lx_l10n_rpk-00000/build/l10n/sk/browser/firefox-l10n.js"
+//@line 4 "/builds/slave/rel-m-esr45_fx_lx_l10n_rpk-000/build/l10n/sk/browser/firefox-l10n.js"
 
-//@line 6 "/builds/slave/rel-m-rel_fx_lx_l10n_rpk-00000/build/l10n/sk/browser/firefox-l10n.js"
+//@line 6 "/builds/slave/rel-m-esr45_fx_lx_l10n_rpk-000/build/l10n/sk/browser/firefox-l10n.js"
 
 pref("general.useragent.locale", "sk");
 pref("browser.fixup.alternate.suffix", ".sk");
