9cacd9a633ee0c13366ec38eaf265c31b06013c6
Kai Hendry <hendry@webconverger.com>
2017-02-11 05:14:00 +0000

    https://www.mozilla.org/en-US/firefox/45.7.0/releasenotes/ security fixes and langpack updates

--- opt/firefox/langpacks/langpack-be@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
+++
@@ -1,6 +0,0 @@
-//@line 4 "/builds/slave/rel-m-esr45_fx_lx_l10n_rpk-000/build/l10n/be/browser/firefox-l10n.js"
-
-//@line 6 "/builds/slave/rel-m-esr45_fx_lx_l10n_rpk-000/build/l10n/be/browser/firefox-l10n.js"
-
-pref("general.useragent.locale", "be");
-pref("browser.fixup.alternate.suffix", ".by");
