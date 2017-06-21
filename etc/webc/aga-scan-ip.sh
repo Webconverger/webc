18e2943c69b184b6d63a56583ec366ff5f6fa64a
aga2017 <root@amag.ru>
2017-06-21 06:50:05 +0000

    add saving AGA_cfg_file and AGA_mod_file into aga-scan-ip.service

--- etc/webc/aga-scan-ip.sh
+++ etc/webc/aga-scan-ip.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+. "/etc/webc/webc.conf"
 AGA_cfg_file="/home/webc/aga.conf"
 AGA_mod_file="/home/webc/aga-mod.conf"
 AGA_int_file="/etc/network/interfaces"
 AGA_int_ori_file="/etc/network/interfaces.ori"
 AGA_resolv_file="/etc/resolv.conf"
 
+AGA_cfg_file_cached="${live_image}/live/aga.conf"
+AGA_mod_file_cached="${live_image}/live/aga-mod.conf"
 
 AGA_net="unknown-net"
 AGA_shop_id="unknown-shop"
 AGA_ID=1
 AGA_mode="kiosk"
 
-. "$AGA_cfg_file"
-. "$AGA_mod_file"
+
+AGA_update_cfg() {
+  # if writable
+  if touch ${live_image}
+    then
+      # Cache cmdline in case subsequent boots can't reach $config_url
+      cp "$AGA_cfg_file" "$AGA_cfg_file_cached"
+      logs "CONFIG: cached $(md5sum $AGA_cfg_file_cached) $(tr '\n' ' ' < $AGA_cfg_file_cached)"
+      # Kicks off an upgrade
+      mkfifo $upgrade_pipe
+    else
+     # ${live_image} could not be made writable (e.g. live version: booting
+     # from an iso fs), so just use the new config downloaded
+     # and skip all the other stuff below
+     logs "CONFIG: Not a writable boot medium. Could not cache configuration nor upgrade."
+  fi
+}
 
 AGA_has_eth0()
 {
@@ -29,6 +47,33 @@ AGA_has_network()
 {
   netstat -rn | grep -qs '^0.0.0.0'
 }
+
+
+# begin
+
+# If we have a "cached" version of the configuration on disk,
+# copy that to /etc/webc, so we can compare the new version with
+# it to detect changes and/or use it in case the new download
+# fails.
+if test -s "$AGA_cfg_file_cached"
+  then
+    cp "$AGA_cfg_file_cached" "$AGA_cfg_file"
+    logs "CONFIG: Applied cache $(md5sum $AGA_cfg_file)"
+  else
+    touch "$AGA_cfg_file"
+    logs "CONFIG: No cache"
+fi
+if test -s "$AGA_mod_file_cached"
+  then
+    cp "$AGA_mod_file_cached" "$AGA_mod_file"
+    logs "CONFIG: Applied cache $(md5sum $AGA_mod_file)"
+  else
+    touch "$AGA_mod_file"
+    logs "CONFIG: No cache"
+fi
+
+. "$AGA_cfg_file"
+. "$AGA_mod_file"
 
 AGA_ip=30
 if (test $AGA_mode = "showcase"); 
@@ -71,6 +116,7 @@ EOF
   ifup eth0
   sleep 5
   if (arp-scan -q -arpspa=172.$AGA_net.$AGA_shop_id.$AGA_ip 172.$AGA_net.$AGA_shop_id.1 | grep -qs "172.$AGA_net.$AGA_shop_id.1") then 
+    AGA_update_cfg
     exit
   fi
 fi
@@ -100,6 +146,7 @@ iface eth0 inet static
 EOF
         echo nameserver 172.$AGA_net.$AGA_shop_id.1 > $AGA_resolv_file
         ifup eth0
+        AGA_update_cfg
         exit
       fi
     done
